ae00d5a6849409c71d8a562942d58ff6cb9d5201
Felix Bernhardt <felix.bernhardt@mailbox.org>
2021-12-22 20:50:15 +0100

    optimize OutlineButton

--- src/widgets/outline_button.rs
+++ src/widgets/outline_button.rs
@@ -4,26 +4,27 @@ use druid::{theme, Affine, Data, Insets};
 
 const LABEL_INSETS: Insets = Insets::uniform_xy(8., 3.);
 
+/// Use two labels internally to avoid re-rendering the text on every change
+/// of `is_hot`.
+///
+/// todo: use specialized label widget, which avoids the `Clone` trait for the text
+/// value passed to the button.
 pub struct OutlineButton<T> {
-    label: Label<T>,
+    active_label: Label<T>,
+    inactive_label: Label<T>,
     label_size: Size,
 }
 
 impl<T: Data> OutlineButton<T> {
-    pub fn new(text: impl Into<LabelText<T>>) -> OutlineButton<T> {
-        OutlineButton::from_label(Label::new(text))
-    }
+    pub fn new(text: impl Into<LabelText<T>> + Clone) -> OutlineButton<T> {
+        let active_label = Label::new(text.clone()).with_text_color(theme::BACKGROUND_DARK);
+        let inactive_label = Label::new(text).with_text_color(theme::FOREGROUND_LIGHT);
 
-    pub fn from_label(label: Label<T>) -> OutlineButton<T> {
         OutlineButton {
-            label,
+            active_label,
+            inactive_label,
             label_size: Size::ZERO,
         }
-    }
-
-    pub fn dynamic(text: impl Fn(&T, &Env) -> String + 'static) -> Self {
-        let text: LabelText<T> = text.into();
-        OutlineButton::new(text)
     }
 
     pub fn on_click(
@@ -53,29 +54,31 @@ impl<T: Data> Widget<T> for OutlineButton<T> {
 
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
         if let LifeCycle::HotChanged(_) = event {
-            if ctx.is_hot() {
-                self.label.set_text_color(env.get(theme::BACKGROUND_DARK));
-            } else {
-                self.label.set_text_color(env.get(theme::FOREGROUND_LIGHT));
-            }
-            ctx.request_layout();
-            //ctx.request_paint();
+            ctx.request_paint();
         }
-        self.label.lifecycle(ctx, event, data, env)
+        self.active_label.lifecycle(ctx, event, data, env);
+        self.inactive_label.lifecycle(ctx, event, data, env);
     }
 
     fn update(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
-        self.label.update(ctx, old_data, data, env)
+        self.active_label.update(ctx, old_data, data, env);
+        self.inactive_label.update(ctx, old_data, data, env);
     }
 
     fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &T, env: &Env) -> Size {
         let padding = Size::new(LABEL_INSETS.x_value(), LABEL_INSETS.y_value());
         let label_bc = bc.shrink(padding).loosen();
-        self.label_size = self.label.layout(ctx, &label_bc, data, env);
+
+        self.label_size = self.active_label.layout(ctx, &label_bc, data, env);
+        assert_eq!(
+            self.inactive_label.layout(ctx, &label_bc, data, env),
+            self.label_size
+        );
+
         // HACK: to make sure we look okay at default sizes when beside a textbox,
         // we make sure we will have at least the same height as the default textbox.
         let min_height = env.get(theme::BORDERED_WIDGET_HEIGHT);
-        let baseline = self.label.baseline_offset();
+        let baseline = self.active_label.baseline_offset();
         ctx.set_baseline_offset(baseline + LABEL_INSETS.y1);
 
         let button_size = bc.constrain(Size::new(
@@ -106,7 +109,12 @@ impl<T: Data> Widget<T> for OutlineButton<T> {
 
         ctx.with_save(|ctx| {
             ctx.transform(Affine::translate(label_offset));
-            self.label.paint(ctx, data, env);
+
+            if is_hot {
+                self.active_label.paint(ctx, data, env);
+            } else {
+                self.inactive_label.paint(ctx, data, env);
+            }
         });
     }
 }
